65488bf6b3728909c88f3bb6ad74e1cfe93f0b6c
CarlosTolenk <carlos.tolentinoe@gmail.com>
2022-08-14 11:13:04 -0400

    DDD-architecture - Add test handler OrderCreated and command

---
+++ test/Context/Order/application/commands/handlers/order-created.handler.spec.ts
@@ -0,0 +1,63 @@
+import { ModuleMetadata, Provider } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+
+import { OrderCreatedHandler } from '../../../../../../src/Context/Order/application/commands/handlers/order-created.handler';
+import { OrderCreated } from '../../../../../../src/Context/Order/application/useCase';
+import { OrderRepository } from '../../../../../../src/Context/Order/domain/Order.repository';
+import { OrderPostgresRepository } from '../../../../../../src/Context/Order/infrastructure/repository/order-postgres.repository';
+import { OrderCreatedCommand } from '../../../../../../src/Context/Order/application/commands/implements';
+import { MotherOrder } from '../../../../../__Mocks__/motherObjects/requests/order-created.request';
+import { InvalidArgumentError } from '../../../../../../src/Context/Common/domain/exception';
+
+describe('OrderCreatedHandler', () => {
+  let handler: OrderCreatedHandler;
+  let service: OrderCreated;
+
+  beforeEach(async () => {
+    const repositoryProvider: Provider = {
+      provide: OrderRepository,
+      useClass: OrderPostgresRepository,
+    };
+    const providers: Provider[] = [
+      OrderCreatedHandler,
+      OrderCreated,
+      repositoryProvider,
+    ];
+    const moduleMetadata: ModuleMetadata = { providers };
+    const testModule = await Test.createTestingModule(moduleMetadata).compile();
+
+    handler = testModule.get<OrderCreatedHandler>(OrderCreatedHandler);
+    service = testModule.get<OrderCreated>(OrderCreated);
+  });
+
+  it('should be defined', () => {
+    expect(handler).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe('execute', () => {
+    it('should execute OrderCreatedCommand correctly', async () => {
+      service.run = jest.fn().mockResolvedValue(Promise.resolve());
+      const command = new OrderCreatedCommand(MotherOrder.createOrderRequest());
+
+      await expect(handler.execute(command)).resolves.toEqual(undefined);
+      expect(service.run).toBeCalledTimes(1);
+      expect(service.run).toHaveBeenCalledWith(
+        MotherOrder.createOrderValueObjectFromDTO(command),
+      );
+    });
+
+    it('should execute OrderCreatedCommand wrong when orderId is not UUID valid', async () => {
+      service.run = jest.fn().mockResolvedValue(Promise.resolve());
+      const command = new OrderCreatedCommand(
+        MotherOrder.createOrderRequestWithUUIInvalid(),
+      );
+
+      await expect(handler.execute(command)).rejects.toEqual(
+        new InvalidArgumentError(
+          `<OrderId> does not allow the value <${command.orderId}>`,
+        ),
+      );
+    });
+  });
+});
--- test/__Mocks__/motherObjects/requests/order-created.request.ts
+++ test/__Mocks__/motherObjects/requests/order-created.request.ts
@@ -1,9 +1,33 @@
 import { OrderCreatedDto } from '../../../../src/Context/Order/interfaces/controller/dtos/order-created.dto';
+import {
+  OrderCustomer,
+  OrderId,
+  OrderStatus,
+  StatusOrderAvailable,
+} from '../../../../src/Context/Order/domain/valueObject';
+import { OrderCreatedCommand } from '../../../../src/Context/Order/application/commands/implements';
 
 export class MotherOrder {
   static createOrderRequest(): OrderCreatedDto {
     return {
       id: '60169eca-03f9-46bd-a2e2-699487cea423',
+      shippingGroups: [
+        {
+          productName: 'the real product',
+          price: 150.45,
+        },
+      ],
+      customer: {
+        name: 'Carlos',
+        lastName: 'Tolentino',
+        phoneNumber: '809-xxx-5656',
+      },
+    };
+  }
+
+  static createOrderRequestWithUUIInvalid(): OrderCreatedDto {
+    return {
+      id: 'invalid',
       shippingGroups: [
         {
           productName: 'the real product',
@@ -27,5 +51,17 @@ export class MotherOrder {
         phoneNumber: '809-xxx-5656',
       },
     };
+  }
+
+  static createOrderValueObjectFromDTO(command: OrderCreatedCommand): any {
+    const orderId = new OrderId(command.orderId);
+    const orderStatus = new OrderStatus(StatusOrderAvailable.RECEIVED);
+    const orderCustomer = new OrderCustomer(
+      command.customer.name,
+      command.customer.lastName,
+      command.customer.phoneNumber,
+    );
+
+    return { orderId, orderStatus, orderCustomer };
   }
 }
