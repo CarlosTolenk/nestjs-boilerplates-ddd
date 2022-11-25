8b6d5dc29d654c59ed1cce1641dd1855a8e3d898
CarlosTolenk <carlos.tolentinoe@gmail.com>
2022-11-25 11:16:20 -0400

    Move interfaces to folder infrastructure

--- test/Context/Order/infrastructure/interfaces/controller/order-created.controller.spec.ts
+++ test/Context/Order/infrastructure/interfaces/controller/order-created.controller.spec.ts
@@ -3,9 +3,9 @@ import { Response } from 'express';
 import { CqrsModule } from '@nestjs/cqrs';
 import { createResponse, MockResponse } from 'node-mocks-http';
 
-import { OrderCreatedController } from '../../../../../src/Context/Order/infrastructure/interfaces/controller';
-import { MotherOrder } from '../../../../__Mocks__/MotherObjects/requests/order-created.request';
-import { OrderCreatedCommand } from '../../../../../src/Context/Order/application/commands/implements';
+import { OrderCreatedController } from '../../../../../../src/Context/Order/infrastructure/interfaces/controller';
+import { MotherOrder } from '../../../../../__Mocks__/MotherObjects/requests/order-created.request';
+import { OrderCreatedCommand } from '../../../../../../src/Context/Order/application/commands/implements';
 
 describe('OrderCreatedController', () => {
   let controller: OrderCreatedController;
--- test/Context/Order/infrastructure/interfaces/controller/order-get.controller.spec.ts
+++ test/Context/Order/infrastructure/interfaces/controller/order-get.controller.spec.ts
@@ -3,9 +3,9 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { createResponse, MockResponse } from 'node-mocks-http';
 import { Response } from 'express';
 
-import { OrderGetController } from '../../../../../src/Context/Order/infrastructure/interfaces/controller';
-import { OrderGetByIdQuery } from '../../../../../src/Context/Order/application/queries/implements/order-get-by-id.query';
-import { Uuid } from '../../../../../src/Context/Common/domain/valueObject/Uuid';
+import { OrderGetController } from '../../../../../../src/Context/Order/infrastructure/interfaces/controller';
+import { OrderGetByIdQuery } from '../../../../../../src/Context/Order/application/queries/implements/order-get-by-id.query';
+import { Uuid } from '../../../../../../src/Context/Common/domain/valueObject/Uuid';
 
 describe('OrderGetController', () => {
   let controller: OrderGetController;
